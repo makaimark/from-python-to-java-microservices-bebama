f68cfcc56fee81b99656b1804580d605f5d9ca44
cickib <bellakocsis06@gmail.com>
2017-01-10 12:02:56 +0100

    currency added to db

--- src/main/resources/public/sql/init_db.sql
+++ src/main/resources/public/sql/init_db.sql
@@ -13,5 +13,6 @@ CREATE TABLE IF NOT EXISTS webshopAnalytics
   visit_start TIMESTAMP,
   visit_end TIMESTAMP,
   location VARCHAR,
-  amount FLOAT NULL
+  amount FLOAT NULL,
+  currency VARCHAR
 );
