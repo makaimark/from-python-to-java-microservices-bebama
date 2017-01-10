f459bd6e60c03e874373ccbe4f6904c41742329f
makaimark <makaimark@gmail.com>
2017-01-10 11:21:18 +0100

    i can send the session end time to the server

--- src/main/java/controller/APIController.java
+++ src/main/java/controller/APIController.java
@@ -1,12 +1,21 @@
 package controller;
 
+import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class APIController {
 
     private String sessionId;
     private String webShopId;
+
+    public ModelAndView renderTest(Request req, Response res) throws Exception {
+        Map<Object, Object> params = new HashMap<>();
+        return new ModelAndView(params, "test");
+    }
 
     public String api(Request request, Response response) {
         webShopId = request.queryParams("webshopId");
@@ -20,7 +29,10 @@ public class APIController {
     }
 
     public String countVisitTime(Request request, Response response) {
-        Integer time = Integer.valueOf(request.queryParams("time"));
+        System.out.println("In visit time");
+        //Integer time = Integer.valueOf(request.queryParams("time"));
+        String time = request.queryParams("time");
+        System.out.println(time);
         sessionId = request.queryParams("sessionId");
         return "";
     }
