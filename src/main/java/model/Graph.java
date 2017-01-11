83bab17d89a2494178dfe0938d86c19229b30bdd
makaimark <makaimark@gmail.com>
2017-01-11 12:03:51 +0100

    graph maker is ready

---
+++ src/main/java/model/Graph.java
@@ -0,0 +1,25 @@
+package model;
+
+import org.apache.http.client.utils.URIBuilder;
+
+import java.net.URISyntaxException;
+
+/**
+ * Created by makaimark on 2017.01.11..
+ */
+public class Graph {
+    public static final String API_URL = "http://chart.apis.google.com/chart?";
+
+    public String buildGraphURL(String size, String name, String values, String partition, String colors) throws URISyntaxException {
+        URIBuilder builder = new URIBuilder(API_URL);
+        builder.addParameter("chs", size);
+        builder.addParameter("chdlp", "b"); // must have :D
+        builder.addParameter("chtt", name);
+        builder.addParameter("chdl", values); // needed format: Asleep|Awake|randomthing
+        builder.addParameter("chd", "t:" + partition); // needed format: 1,89,10 ( 100 max)
+        builder.addParameter("cht", "p"); // must have :D
+        builder.addParameter("chco", colors); // needed format: 7D858F,586F8E,7D858F
+
+        return String.valueOf(builder.build());
+    }
+}
