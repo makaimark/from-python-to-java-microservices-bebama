05171d943302fea8d7d10910341779ee00944dcd
cickib <bellakocsis06@gmail.com>
2017-01-11 14:29:52 +0100

    null values not allowed for time and sessionId

--- src/main/resources/public/sql/init_db.sql
+++ src/main/resources/public/sql/init_db.sql
@@ -9,10 +9,10 @@ CREATE TABLE IF NOT EXISTS webshopAnalytics
 (
   an_id SERIAL PRIMARY KEY,
   webshop_id INT REFERENCES webshop(ws_id),
-  session_id VARCHAR,
-  visit_start TIMESTAMP,
-  visit_end TIMESTAMP NULL,
-  location VARCHAR,
+  session_id VARCHAR NOT NULL,
+  visit_start TIMESTAMP NOT NULL,
+  visit_end TIMESTAMP NOT NULL,
+  location VARCHAR NOT NULL,
   amount FLOAT NULL,
   currency VARCHAR NULL
 );
